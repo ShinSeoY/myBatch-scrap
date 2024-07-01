2ecf9f9857e3ff1c625aa7b1ac8d6dd14a18c044
ShinSeoY <tjdudtls312@gmail.com>
2024-07-01 14:30:45 +0900

    - 카프카부터 airflow까지 예제 파일

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,30 +1,70 @@
 from kafka import KafkaProducer
 from json import dumps
-import time
+import requests
+from bs4 import BeautifulSoup
+import json
 
-isLocal = False
-hostname = ''
-if(isLocal):
-    hostname = 'localhost'
-else:
-    hostname = '13.60.3.214'
+# Kafka 설정
+isLocal = True
+hostname = 'localhost' if isLocal else '13.60.3.214'
 
+# brokers = [f'{hostname}:9091', f'{hostname}:9092', f'{hostname}:9093']
+brokers = ['kafka1:19091', 'kafka2:19092', 'kafka3:19093']
 
-brokers = [hostname + ':9091', hostname + ':9092', hostname + ':9093']
 topic = 'my-topic'
 
 producer = KafkaProducer(
     acks=1,
-    compression_type='gzip', # 메시지 전달할 때 압축(None, gzip, snappy, lz4 등)
-    bootstrap_servers=brokers, # 전달하고자 하는 카프카 브로커의 주소 리스트
-    value_serializer=lambda x:dumps(x).encode('utf-8') # 메시지의 값 직렬화
+    compression_type='gzip',  # 메시지 전달할 때 압축(None, gzip, snappy, lz4 등)
+    bootstrap_servers=brokers,  # 전달하고자 하는 카프카 브로커의 주소 리스트
+    value_serializer=lambda x: dumps(x).encode('utf-8')  # 메시지의 값 직렬화
 )
 
-start = time.time()
+# 웹페이지에서 데이터 가져오기
+def get_webpage(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.content
+        else:
+            print(f"Failed to retrieve webpage. Status code: {response.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Error during requests to {url}: {e}")
+        return None
 
-for i in range(100):
-    data = {'str' : 'result'+str(i)}
-    producer.send(topic, value=data)
-    producer.flush()
- 
-print('[Done Time]:', time.time()-start)
+# JSON으로 데이터 추출
+def extract_information(html_content):
+    if html_content is None:
+        return None
+    
+    try:
+        soup = BeautifulSoup(html_content, 'html.parser')
+        script_tag = soup.find('script', id='__NEXT_DATA__')
+        
+        if script_tag:
+            data = json.loads(script_tag.string)
+            exchange_data = data['props']['pageProps']['dehydratedState']['queries'][2]['state']['data']['result']
+            return exchange_data
+    except Exception as e:
+        print(f"Error during parsing: {e}")
+        return None
+
+# 메인 함수
+def main():
+    url = 'https://m.stock.naver.com/marketindex/home/exchangeRate/exchange'
+    html_content = get_webpage(url)
+    if html_content:
+        extracted_data = extract_information(html_content)
+        if extracted_data:
+            print(extracted_data)
+            producer.send(topic, value=extracted_data)
+            producer.flush()
+            print('[Data sent to Kafka]')
+        else:
+            print("Failed to extract data from the webpage.")
+    else:
+        print("Failed to retrieve the webpage.")
+
+if __name__ == "__main__":
+    main()
--- test-dag.py
+++ test-dag.py
@@ -3,6 +3,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
+import subprocess
 
 # 디폴트 설정
 default_args = {
@@ -16,80 +17,24 @@ default_args = {
 def _first_step():
     print("first_step")
 
+def _run_producer():
+    result = subprocess.run(['python3', '/opt/airflow/dags/kafka/producer.py'], capture_output=True, text=True)
+    print(result.stdout)
+    if result.stderr:
+        print(result.stderr)
+
 def _complete():
     print("완료~~~~~~")
 
 # DAG 틀 설정
 with DAG(
-    dag_id="test-dag-01",
+    dag_id="test-dag-02",
     schedule="* * * * *", 
     default_args=default_args,
     # tags=["naver", "search", "local", "api", "pipeline"],
     # catchup을 True로 하면, start_date 부터 현재까지 못돌린 날들을 채운다
     # catchup=False
     ) as dag:
-
-    # 네이버 API로 지역 식당을 검색할 것이다. 
-    # 지역 식당명, 주소, 카테고리, 설명, 링크를 저장할 것이므로 다음과 같이 테이블을 구성한다.
-    # creating_table = SqliteOperator(
-    #     task_id="creating_table",
-    #     sqlite_conn_id="db_sqlite", # 웹UI에서 connection을 등록해줘야 함.
-    #     # naver_search_result 라는 테이블이 없는 경우에만 만들도록 IF NOT EXISTS 조건을 넣어주자.
-    #     sql='''
-    #         CREATE TABLE IF NOT EXISTS naver_search_result( 
-    #             title TEXT,
-    #             address TEXT,
-    #             category TEXT,
-    #             description TEXT,
-    #             link TEXT
-    #         )
-    #     '''
-    # )
-    # HTTP 센서를 이용해 응답 확인 (감지하는 오퍼레이터로 실제 데이터를 가져오는 것 X)
-    # is_api_available = HttpSensor(
-    #     task_id="is_api_available",
-    #     http_conn_id="naver_search_api",
-    #     endpoint="v1/search/local.json", # url - uri에서 Host 부분을 제외한 파트(~.com 까지가 host)
-    #     # 요청 헤더, -H 다음에 오는 내용들
-    #     headers={
-    #         "X-Naver-Client-Id" : f"{NAVER_CLI_ID}",
-    #         "X-Naver-Client-Secret" : f"{NAVER_CLI_SECRET}",
-    #     },
-    #     request_params={
-    #         "query": "김치찌개",
-    #         "display": 5
-    #     }, # 요청 변수
-    #     response_check=lambda response: response.json() # 응답 확인
-    # )
-    
-    # 네이버 검색 결과를 가져올 오퍼레이터를 만든다.
-    # crawl_naver = SimpleHttpOperator(
-    #     task_id="crawl_naver",
-    #     http_conn_id="naver_search_api",
-    #     endpoint="v1/search/local.json", # url 설정
-    #     headers={
-    #         "X-Naver-Client-Id" : f"{NAVER_CLI_ID}",
-    #         "X-Naver-Client-Secret" : f"{NAVER_CLI_SECRET}",
-    #     }, # 요청 헤더
-    #     data={
-    #         "query": "김치찌개",
-    #         "display": 5
-    #     }, # 요청 변수
-    #     method="GET", # 통신 방식 GET, POST 등등 맞는 것으로
-    #     response_filter=lambda res : json.loads(res.text),
-    #     log_response=True
-    # )
-        # 검색 결과 전처리하고 CSV 저장
-    # preprocess_result = PythonOperator(
-    #         task_id="preprocess_result",
-    #         python_callable=preprocessing # 실행할 파이썬 함수
-    # )
-
-    # csv 파일로 저장된 것을 테이블에 저장
-    # store_result = BashOperator(
-    #     task_id="store_naver",
-    #     bash_command='echo -e ".separator ","\n.import /home/kurran/airflow/dags/data/naver_processed_result.csv naver_search_result" | sqlite3 /home/kurran/airflow/airflow.db'
-    # )
 
     # 대그 완료 출력
     print_complete = PythonOperator(
@@ -102,6 +47,11 @@ with DAG(
             python_callable=_first_step # 실행할 파이썬 함수
     )
     
+    run_producer = PythonOperator(
+        task_id='run_producer',
+        python_callable=_run_producer, # 실행할 파이썬 함수
+    )
+    
     print_date = BashOperator(
         task_id='print_date',
         bash_command='echo "~~~~~~~~Current date is $(date)"',
@@ -110,4 +60,4 @@ with DAG(
     
     # 파이프라인 구성하기
     # creating_table >> is_api_available >> crawl_naver >> preprocess_result >> store_result >> print_complete
-    print_date >> print_first >> print_complete
+    print_date >> print_first >> run_producer >> print_complete
