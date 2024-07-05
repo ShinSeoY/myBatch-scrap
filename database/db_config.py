7870429939e5ac569fa4d270ea3691685a0d9780
ShinSeoY <tjdudtls312@gmail.com>
2024-07-05 18:01:58 +0900

    - db 설정 .env로 변경

--- database/db_config.py
+++ database/db_config.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+# .env 파일 로드 (경로를 정확히 지정)
+load_dotenv(dotenv_path='../../.env')
 
 db_config = {
-    'host': '127.0.0.1',
-    'database': 'MY_EXCHANGE',
-    'user': 'root',
-    'password': 'nextage2020!'
+    'host': os.getenv('DB_HOST', '127.0.0.1'),  # 기본값 설정
+    'database': os.getenv('DB_NAME', 'MY_EXCHANGE'),
+    'user': os.getenv('DB_USER', 'root'),
+    'password': os.getenv('DB_PASSWORD', 'nextage2020!')
 }
 
 DATABASE_URL = f"mysql+pymysql://{db_config['user']}:{db_config['password']}@{db_config['host']}/{db_config['database']}"
