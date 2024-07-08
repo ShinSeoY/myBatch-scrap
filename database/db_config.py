9fcc69a5b812209157ccab2875f12472f107b4f7
ShinSeoY <tjdudtls312@gmail.com>
2024-07-08 18:15:32 +0900

    - db config 변경

--- database/db_config.py
+++ database/db_config.py
@@ -9,10 +9,13 @@ db_config = {
     'host': os.environ.get('DB_HOST'),
     'database': os.environ.get('DB_NAME'),
     'user': os.environ.get('DB_USER'),
-    'password': os.environ.get('DB_PASSWORD')
+    'password': os.environ.get('DB_PASSWORD'),
+    'port': os.environ.get('DB_PORT')
+
 }
 
-DATABASE_URL = f"mysql+pymysql://{db_config['user']}:{db_config['password']}@{db_config['host']}/{db_config['database']}"
+DATABASE_URL = f"mysql+pymysql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
+# DATABASE_URL = "mysql+pymysql://admin:nextage2020!@my-exchange.cpa8g4o0appv.eu-north-1.rds.amazonaws.com:3306/MY_EXCHANGE"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
